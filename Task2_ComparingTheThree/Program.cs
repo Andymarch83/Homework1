94d0c973c2d4d5f7d81dd49aeb1109119ae2390c
Andrey <5657031@gmail.com>
2023-01-26 20:11:35 +0300

    Task2_ComparingTheThree

---
+++ Task2_ComparingTheThree/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+string numberOneStr = Console.ReadLine();
+int numberOne = Convert.ToInt32(numberOneStr);
+Console.WriteLine("Введите второе число: ");
+string numberTwoStr = Console.ReadLine();
+int numberTwo = Convert.ToInt32(numberTwoStr);
+Console.WriteLine("Введите третье число: ");
+string numberThreeStr = Console.ReadLine();
+int numberThree = Convert.ToInt32(numberThreeStr);
+int max = numberOne;
+if(numberTwo > max )
+{
+ max = numberTwo;
+if(numberThree > max)
+max = numberThree;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
