ceec47a95d8ae74e6810b686d7e3d975c99e74d0
Andrey <5657031@gmail.com>
2023-01-26 20:35:55 +0300

    Task3_EvenNumbered

---
+++ Task3_EvenNumbered/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+if((number%2) == 0)
+{
+Console.WriteLine("чётное");
+}
+else
+{
+  Console.WriteLine("нечётное");  
+}
