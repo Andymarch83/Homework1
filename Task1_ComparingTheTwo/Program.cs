da72fd6edc14600c4168e79bd9a5946bc4622c20
Andrey <5657031@gmail.com>
2023-01-26 12:13:56 +0300

    Task1_ComparingTheTwo

---
+++ Task1_ComparingTheTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+string numberOneStr = Console.ReadLine();
+int numberOne = Convert.ToInt32(numberOneStr);
+Console.WriteLine("Введите второе число: ");
+string numberTwoStr = Console.ReadLine();
+int numberTwo = Convert.ToInt32(numberTwoStr);
+if(numberOne > numberTwo)
+{
+    Console.Write("max = " );
+    Console.WriteLine(numberOne);
+}
+else 
+Console.Write("max = ");
+Console.WriteLine(numberTwo);
