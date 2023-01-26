6e7af0680c1ff5064962dac7ab6aa30325fc8415
Andrey <5657031@gmail.com>
2023-01-26 20:56:15 +0300

    Task4_EvenNumberedToN

---
+++ Task4_EvenNumberedToN/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+for (int i = 2; i <= number; i++)
+if ((i % 2) == 0)
+{
+  Console.Write($"{i},");
+}
+
