d5e36f86f931bd217a451331843ea02f1fafae86
Andrey <5657031@gmail.com>
2023-01-27 15:03:37 +0300

    Fixed lines changes

--- Task1_ComparingTheTwo/Program.cs
+++ Task1_ComparingTheTwo/Program.cs
@@ -1,14 +1,19 @@
 ﻿Console.WriteLine("Введите первое число: ");
+
 string numberOneStr = Console.ReadLine();
 int numberOne = Convert.ToInt32(numberOneStr);
+
 Console.WriteLine("Введите второе число: ");
+
 string numberTwoStr = Console.ReadLine();
 int numberTwo = Convert.ToInt32(numberTwoStr);
+
 if(numberOne > numberTwo)
 {
     Console.Write("max = ");
     Console.WriteLine(numberOne);
 }
+
 else
 {
     Console.Write("max = ");
--- Task4_EvenNumberedToN/Program.cs
+++ Task4_EvenNumberedToN/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите число N: ");
+
 string numberStr = Console.ReadLine();
 int number = Convert.ToInt32(numberStr);
+
 for (int i = 2; i <= number; i++)
 if ((i % 2) == 0)
 {
